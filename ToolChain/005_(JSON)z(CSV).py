762a31cb010c2496ada3b825f1f76f2b4046424b
dappnelson <bankscar000@gmail.com>
2013-12-31 23:52:53 -0500

    23:32

--- ToolChain/005_(JSON)z(CSV).py
+++ ToolChain/005_(JSON)z(CSV).py
@@ -1,24 +1,12 @@
 input_path = '004_(JSON).txt'
 output_path = '006_(CSV).txt'
-#d1 = 'points = '
 
-#with open(input_path, "r") as f:
-#    with open(output_path, "w") as g:
-#        for line in input_path:
-#            Data = f.readline()
-#            Data = str(Data)
-#            print Data
-#            post_d1 = Data.partition(d1)[2]
-#            g.write(post_d1)
-			
-import json
-import csv
 
-f = open(input_path)
-data = json.load(f)
-f.close()
+with open(input_path, 'r') as f:
+lines = f.readlines()
+datastring = lines[0].strip()
+data = json.loads(datastring)
 
-f=csv.writer(open(output_path,'wb+'))
-
-for item in data:
-  f.writerow([item['pk'], item['model']] + item['fields'].values())
+with open(output_path, 'w') as g:
+for line in data:
+	output_path.write(str(line[0]) + ', ' + str(line[1]) + ', ' + str(line[2]))
