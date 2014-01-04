cd2d6d12f73f84ba1d24124f8dccb6d2e713216e
dappnelson <bankscar000@gmail.com>
2014-01-03 21:34:31 -0500

    21:34

--- ToolChain/103_(LOOPS)z(Z-TRANS).py
+++ ToolChain/103_(LOOPS)z(Z-TRANS).py
@@ -7,14 +7,15 @@ input_dir = '102_(LOOPS)'
 z_translate = float(raw_input('Enter dZ for translation: \n\n >>>'))
 
 for filename in glob.glob(input_dir + '/*.txt'):
-	write_test = filename + '.test'
-	with open(filename, 'r') as f:
-		with open(write_test , 'w') as g:
-			data = []
-			for line in f:
-				data.append(json.loads('[' + line.strip() + ']'))
-			for element in data:
-				g.write(str(element[0]) + ', ' + str(element[1]) + ', ' + str(element[2]+z_translate) + '\n')
-				element = line.strip()
-				print element
-			os.startfile(write_test, 'open')
+    write_test = filename + '.test'
+    with open(filename, 'r') as f:
+        with open(write_test , 'w') as g:
+            data = []
+            for line in f:
+                data.append(json.loads('[' + line.strip() + ']'))
+            for element in data:
+                g.write(str(element[0]) + ', ' + str(element[1]) + ', ' + str(element[2]+z_translate) + '\n')
+                element = line.strip()
+                print element
+            os.system(str(write_test))
+            #os.startfile(write_test, 'open')
